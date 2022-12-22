a48c5f6c26230cd2bc217e7a76f6f7aedd36db12
mariago6 <merigon_93@hotmail.com>
2022-12-22 16:28:18 +0100

    row container

--- src/components/Buttons/DeleteButton/DeleteButton.styled.js
+++ src/components/Buttons/DeleteButton/DeleteButton.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const RemoveButton = styled.button`
-  color: white;
+  color: black;
   background-color: transparent; 
   border: none; 
   cursor: pointer;
--- src/components/ItemList/ItemList.styled.js
+++ src/components/ItemList/ItemList.styled.js
@@ -3,6 +3,12 @@ import styled from "styled-components";
 export const ItemRow = styled.div`
   display: flex; 
   justify-content: center; 
+  border: solid;
+  border-color: #00FFAB; 
+  height: 40px; 
+  margin: 5px;
+  background-color: white;  
+  color: black; 
 `
 
 export const ItemLabel = styled.label`
